37c77619fb463036869120c9fcdec4b7fc4b69cc
Ernesto Canquiz <fdsoil123@gmail.com>
2020-05-30 10:01:52 -0400

    Lesson 9 - setLastName & getLastName

--- 6/app/User.php
+++ 6/app/User.php
@@ -4,15 +4,26 @@ namespace App;
 class User
 {
     protected $name = "";
+    protected $lastname = "";
 
     public function setName($name)
     {
         $this->name = $name;
     }
 
+    public function setLastName($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
     public function getName()
     {
         return $this->name;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastname;
     }
 
 
--- 6/tests/UserTest.php
+++ 6/tests/UserTest.php
@@ -6,12 +6,27 @@ use App\User;
 class UserTest extends TestCase
 {
 
+    protected $user;
+
+    public function setUp() //: void 
+    {
+        $this->user = new User;
+        //Does not work with ':void' with PHP versions less than 7.1
+        //You can use 'return;' but it is not necessary
+    }
+
     public function test_i_can_the_name()
     {
-        $user = new User;
-        $user->setName("Ernesto");
+        $this->user->setName("Ernesto");
 
-        $this->assertEquals($user->getName(), "Ernesto");
+        $this->assertEquals($this->user->getName(), "Ernesto");
+    }
+
+    public function test_i_can_the_lastname()
+    {
+        $this->user->setLastName("Canquiz");
+
+        $this->assertEquals($this->user->getLastName(), "Canquiz");
     }
 
 }
